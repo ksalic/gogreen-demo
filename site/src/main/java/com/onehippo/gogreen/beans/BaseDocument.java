bca192a6404e7d8536d2ef7ae5b75e0f178601cd
Woonsan Ko <woonsan.ko@bloomreach.com>
2015-11-04 09:31:16 +0000

    SALESDEMO-173 code formatting

--- site/src/main/java/com/onehippo/gogreen/beans/BaseDocument.java
+++ site/src/main/java/com/onehippo/gogreen/beans/BaseDocument.java
@@ -26,44 +26,48 @@ import org.hippoecm.repository.api.HippoNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Node(jcrType="hippogogreen:basedocument")
+@Node(jcrType = "hippogogreen:basedocument")
 public class BaseDocument extends HippoDocument {
 
     public static final Logger log = LoggerFactory.getLogger(BaseDocument.class);
 
     private String localizedName = null;
-    
+
     public String getLocalizedName() {
-        if(localizedName == null) {
+        if (localizedName == null) {
             try {
-                javax.jcr.Node canonical = ((HippoNode)this.getNode()).getCanonicalNode();
-                if(canonical == null) {
+                javax.jcr.Node canonical = ((HippoNode) this.getNode()).getCanonicalNode();
+                if (canonical == null) {
                     // for a virtual only node, the local name is just the node name itself
                     localizedName = this.getName();
                 } else {
-                    localizedName = ((HippoNode)canonical).getLocalizedName();
+                    localizedName = ((HippoNode) canonical).getLocalizedName();
                 }
             } catch (RepositoryException e) {
-               log.warn("RepositoryException for localized name", e);
-            }  
+                log.warn("RepositoryException for localized name", e);
+            }
         }
         return localizedName;
     }
-    
-    public String getLastModifiedBy(){
+
+    public String getLastModifiedBy() {
         return getProperty("hippostdpubwf:lastModifiedBy");
     }
-    public String getCreatedBy(){
+
+    public String getCreatedBy() {
         return getProperty("hippostdpubwf:createdBy");
     }
-    public Calendar getLastModificationDate(){
+
+    public Calendar getLastModificationDate() {
         return getProperty("hippostdpubwf:lastModificationDate");
     }
-    public Calendar getCreationDate(){
+
+    public Calendar getCreationDate() {
         return getProperty("hippostdpubwf:creationDate");
     }
-    public Calendar getPublicationDate(){
+
+    public Calendar getPublicationDate() {
         return getProperty("hippostdpubwf:publicationDate");
     }
-    
+
 }
