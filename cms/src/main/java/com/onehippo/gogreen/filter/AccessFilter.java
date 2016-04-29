4aa970382283eff22e432756cef61de4923b525a
Marijan Milicevic <marijan.milicevic@bloomreach.com>
2016-04-29 14:10:37 +0000

    SALESDEMO-259 Shield potential harmful services from outside for OD2 deploy - add access filter

---
+++ cms/src/main/java/com/onehippo/gogreen/filter/AccessFilter.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2016 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.onehippo.gogreen.filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.api.client.repackaged.com.google.common.base.Strings;
+import com.google.common.base.Splitter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class AccessFilter implements Filter {
+
+    private static final Logger log = LoggerFactory.getLogger(AccessFilter.class);
+    private static final Splitter COMMA_SPLITTER = Splitter.on(',').trimResults().omitEmptyStrings();
+    private Collection<IpMatcher> configuration;
+
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+        configuration = new ArrayList<>();
+        final String parameter = filterConfig.getInitParameter("allowed-ip-ranges");
+        if (!Strings.isNullOrEmpty(parameter)) {
+            final Iterable<String> ranges = COMMA_SPLITTER.split(parameter);
+            for (String range : ranges) {
+                configuration.add(IpMatcher.valueOf(range));
+            }
+        }
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        if (allowed((HttpServletRequest)request)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        final HttpServletResponse resp = (HttpServletResponse)response;
+        resp.sendRedirect("/");
+
+    }
+
+    private boolean allowed(final HttpServletRequest request) {
+        final String ip = getIp(request);
+        if (Strings.isNullOrEmpty(ip)) {
+            return false;
+        }
+        log.debug("IP found for '{}': {}", request.getPathInfo(), ip);
+        final Iterator<IpMatcher> i = configuration.iterator();
+        try {
+            while (i.hasNext()) {
+                final IpMatcher ipMatcher = i.next();
+                boolean match = ipMatcher.matches(ip);
+                if (match) {
+                    log.debug("IP {} matching rule {}", ip, ipMatcher);
+                    return true;
+                } else {
+                    log.trace("IP {} NOT matching rule {}", ip, ipMatcher);
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            log.warn("Invalid IP address: {}", ip);
+        }
+
+        return false;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    private static String getIp(HttpServletRequest request) {
+        if (request != null) {
+            final String header = request.getHeader("X-Forwarded-For");
+            if (Strings.isNullOrEmpty(header)) {
+                return request.getRemoteAddr();
+            }
+            final Iterable<String> ipAddresses = COMMA_SPLITTER.split(header);
+            final Iterator<String> iterator = ipAddresses.iterator();
+            if (iterator.hasNext()) {
+                return iterator.next();
+            }
+            return request.getRemoteAddr();
+        }
+        return null;
+    }
+
+}
---
+++ cms/src/main/java/com/onehippo/gogreen/filter/IpMatcher.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2016 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.onehippo.gogreen.filter;
+
+import com.google.common.base.Objects;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+
+
+/**
+ * Checks if IP addresses matches with the IP range. Relies on org.springframework.security.web.util.IpAddressMatcher
+ *
+ * @author Mickael Tricot
+ * @version $Id$
+ */
+public final class IpMatcher {
+
+    /**
+     * Returns the IP matcher representation of the IP range argument
+     *
+     * @param ipRange IP range
+     * @return IP matcher
+     */
+    public static IpMatcher valueOf(String ipRange) {
+        String ipRangeTrimmedToNull = StringUtils.trimToNull(ipRange);
+        if (ipRangeTrimmedToNull == null) {
+            return null;
+        }
+        return new IpMatcher(ipRangeTrimmedToNull);
+    }
+
+    /**
+     * IP matcher
+     */
+    private final IpAddressMatcher ipMatcher;
+
+    /**
+     * IP range
+     */
+    private final String ipRange;
+
+    /**
+     * Constructor
+     *
+     * @param ipRange IP range
+     * @throws IllegalArgumentException If the IP range is malformed
+     */
+    private IpMatcher(String ipRange) {
+        ipMatcher = new IpAddressMatcher(ipRange);
+        this.ipRange = ipRange;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+        return this == o || o instanceof IpMatcher && Objects.equal(getIpRange(), ((IpMatcher)o).getIpRange());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getIpRange());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return getIpRange();
+    }
+
+    /**
+     * IP range getter
+     *
+     * @return IP range
+     */
+    public String getIpRange() {
+        return ipRange;
+    }
+
+    /**
+     * Checks if the IP range matches the IP address
+     *
+     * @param ipAddress IP address
+     * @return TRUE if the IP is in the IP range
+     * @throws IllegalArgumentException If the IP is malformed
+     */
+    public boolean matches(String ipAddress) {
+        String ipAddressTrimmed = StringUtils.trimToNull(ipAddress);
+        return ipAddressTrimmed != null && ipMatcher.matches(ipAddressTrimmed);
+    }
+}
