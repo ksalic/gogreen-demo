a890f423a8966547cea51be5450280ecbf9f332d
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2016-05-09 20:13:53 +0000

    SALESDEMO-259: added localhost IPv6 in config; enabled the filter; changed copyright notice (not open source)

--- cms/src/main/java/com/onehippo/gogreen/filter/AccessFilter.java
+++ cms/src/main/java/com/onehippo/gogreen/filter/AccessFilter.java
@@ -1,19 +1,6 @@
 /*
  * Copyright 2016 Hippo B.V. (http://www.onehippo.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
-
 package com.onehippo.gogreen.filter;
 
 import java.io.IOException;
@@ -30,14 +17,15 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+/**
+ * Filter allowing only access for IP ranges that are configured  by init-parameter 'allowed-ip-ranges'.
+ */
 public class AccessFilter implements Filter {
 
     private static final Logger log = LoggerFactory.getLogger(AccessFilter.class);
