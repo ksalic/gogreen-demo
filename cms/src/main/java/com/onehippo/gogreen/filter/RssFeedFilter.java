ed61d96b03c1d465448a405c83cb8c5b808aedaa
Marijan Milicevic <marijan.milicevic@bloomreach.com>
2017-01-27 15:21:19 +0000

    SALESDEMO-299 RSS feeds no longer loading on CMS login page - remove dependency on external feed parsers - add simple server side proxy filter - parse XML with Jquery

---
+++ cms/src/main/java/com/onehippo/gogreen/filter/RssFeedFilter.java
@@ -0,0 +1,59 @@
+package com.onehippo.gogreen.filter;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.apache.commons.io.IOUtils;
+import org.apache.wicket.util.string.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class RssFeedFilter implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(RssFeedFilter.class);
+    private OkHttpClient client;
+
+
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+        client = new OkHttpClient();
+    }
+
+    @Override
+    public void doFilter(final ServletRequest req, final ServletResponse resp, final FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse r = (HttpServletResponse) resp;
+        final String url = req.getParameter("feed");
+
+        if (Strings.isEmpty(url)) {
+            r.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        Response response = null;
+        try {
+            final Request request = new Request.Builder().url(url).build();
+            response = client.newCall(request).execute();
+            if (response.code() != HttpServletResponse.SC_OK) {
+                r.setStatus(response.code());
+                return;
+            }
+            final String result = response.body().string();
+            final PrintWriter writer = r.getWriter();
+            writer.write(result);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            log.error("", e);
+            r.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
+        } finally {
+            IOUtils.closeQuietly(response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
--- cms/src/main/resources/skin/js/rss-info.js
+++ cms/src/main/resources/skin/js/rss-info.js
@@ -1,72 +1,50 @@
 /*
- * Copyright 2012-2013 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2012-2017 Hippo B.V. (http://www.onehippo.com)
  */
 "use strict";
-
-(function(google, $) {
-
-    google.load("feeds", "1");
-
-    function initialize() {
-        $("#feeds").hide();
-        var feed = new google.feeds.Feed("http://www.onehippo.com/en/events-rss");
-        feed.setNumEntries(1);
-        feed.load(function (result) {
-            if (!result.error) {
-                appendEntries(result, "events");
-            } else {
-                $("#events").hide();
-            }
+$(document).ready(function() {
+  var addRandom = true;
+  var randomAuthor = "author" + (addRandom ? randomWithZeros(20) : '');
+  var randomEditor = "editor" + (addRandom ? randomWithZeros(20) : '');
+  var randomAdmin = "admin" + (addRandom ? randomWithZeros(20) : '');
+  $("#RandomAuthor").append(randomAuthor + " - " + randomAuthor);
+  $("#RandomEditor").append(randomEditor + " - " + randomEditor);
+  $("#RandomAdmin").append(randomAdmin + " - " + randomAdmin);
+  function randomWithZeros(upperLimit) {
+    var randomNumber = Math.ceil(Math.random() * upperLimit);
+    return (randomNumber < 10) ? "0" + randomNumber : "" + randomNumber;
+  }
+  function fetchFeed(feed, name, limit) {
+    $.ajax(feed, {
+      accepts: {
+        xml: "application/rss+xml"
+      },
+      dataType: "xml",
+      success: function(data) {
+        var counter = 0;
+        $(data).find("item").each(function() {
+          if (counter < limit) {
+            appendEntry($(this), name);
+            counter++;
+          }
         });
-        feed = new google.feeds.Feed("http://www.onehippo.com/en/news-rss");
-        feed.setNumEntries(1);
-        feed.load(function (result) {
-            if (!result.error) {
-                appendEntries(result, "news");
-            } else {
-                $("#news").hide();
-            }
-        });
-        feed = new google.feeds.Feed("http://www.onehippo.com/en/blogs-rss");
-        feed.setNumEntries(3);
-        feed.load(function (result) {
-            if (!result.error) {
-                appendEntries(result, "blogs");
-            } else {
-                $("#blogs").hide();
-            }
-        })
-    }
-
-    function appendEntries(result, containerId) {
-        var container = $("#" + containerId);
-        for (var i = 0; i < result.feed.entries.length; i++) {
-            var entry = result.feed.entries[i];
-            var title = '<h3>' + entry.title + '</h3>';
-            var docDate = '<span>' + entry.publishedDate + '</span>';
-            var description = '<p>' + entry.contentSnippet + '...</p>';
-            var item = '<li><a href="' + entry.link + '">' + title + docDate + description + '</a></li>';
-            container.append(item);
-        }
-        $("#feeds").show();
-    }
-
-    google.setOnLoadCallback(initialize);
-
-    $(document).ready(function () {
-        //var domain = document.location.href;
-        var addRandom = true;//domain.indexOf('onehippo.com') > -1;
-        var randomAuthor = "author" + (addRandom ? randomWithZeros(20) : '');
-        var randomEditor = "editor" + (addRandom ? randomWithZeros(20) : '');
-        var randomAdmin = "admin" + (addRandom ? randomWithZeros(20) : '');
-        $("#RandomAuthor").append(randomAuthor + " - " + randomAuthor);
-        $("#RandomEditor").append(randomEditor + " - " + randomEditor);
-        $("#RandomAdmin").append(randomAdmin + " - " + randomAdmin);
+      },
+      error: function() {
+        $("#" + name).hide();
+      }
     });
-    
-    function randomWithZeros(upperLimit) {
-        var randomNumber = Math.ceil(Math.random() * upperLimit);
-        return (randomNumber < 10) ? "0" + randomNumber : ""+randomNumber;
-    }
+  }
+  fetchFeed("rss/?feed=http://www.onehippo.com/en/events-rss", "events", 1);
+  fetchFeed("rss/?feed=http://www.onehippo.com/en/news-rss", "news", 1);
+  fetchFeed("rss/?feed=http://www.onehippo.com/en/blogs-rss", "blogs", 3);
+});
+function appendEntry(el, containerId) {
+  var container = $("#" + containerId);
 
-})(google, jQuery);
+  var title = '<h3>' + el.find("title").text() + '</h3>';
+  var docDate = '<span>' + el.find("pubDate").text() + '</span>';
+  var description = '<p>' + el.find("description").text() + '...</p>';
+  var item = '<li><a href="' + el.find("link").text() + '">' + title + docDate + description + '</a></li>';
+  container.append(item);
+  $("#feeds").show();
+}
