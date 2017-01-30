d05558dc18737b2c31a96a6b1e24bcadecfb73f6
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2017-01-30 13:55:58 +0000

    SALESDEMO-299 remove okhttp in favor of already used com.sun.syndication

--- cms/src/main/java/com/onehippo/gogreen/filter/RssFeedFilter.java
+++ cms/src/main/java/com/onehippo/gogreen/filter/RssFeedFilter.java
@@ -4,32 +4,35 @@
 package com.onehippo.gogreen.filter;
 
 import com.sun.syndication.feed.synd.SyndFeed;
-import okhttp3.OkHttpClient;
-import com.sun.syndication.io.*;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.apache.commons.io.IOUtils;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.SyndFeedOutput;
+import com.sun.syndication.io.XmlReader;
+
 import org.apache.wicket.util.string.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.StringReader;
+import java.net.URL;
 
 /**
  * Filter that proxies /rss/* requests based on the 'feed' parameter.
  */
 public class RssFeedFilter implements Filter {
     private static final Logger log = LoggerFactory.getLogger(RssFeedFilter.class);
-    private OkHttpClient client;
 
 
     @Override
     public void init(final FilterConfig filterConfig) throws ServletException {
-        client = new OkHttpClient();
     }
 
     @Override
@@ -42,32 +45,23 @@ public class RssFeedFilter implements Filter {
             r.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
-        Response response = null;
         try {
-            final Request request = new Request.Builder().url(url).build();
-            response = client.newCall(request).execute();
-            log.debug("Response code from URL is '{}'", response.code());
-            if (response.code() != HttpServletResponse.SC_OK) {
-                r.setStatus(response.code());
-                return;
-            }
-            final String result = response.body().string();
-            // make sure we only fetch RSS feeds (to avoid phishing-alike attempts)
+
+            // make sure we only fetch XML RSS feeds (to avoid phishing-alike attempts)
             final SyndFeedInput feedInput = new SyndFeedInput();
-            final SyndFeed feed = feedInput.build(new StringReader(result));
-            if (feed==null || feed.getEntries() == null || feed.getEntries().size() < 1) {
-                r.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            final SyndFeed feed = feedInput.build(new XmlReader(new URL(url)));
+            if (feed.getEntries() == null || feed.getEntries().size() < 1) {
+                log.warn("No entries from URL '{}'", url);
+                r.setStatus(HttpServletResponse.SC_NOT_FOUND);
                 return;
             }
             final PrintWriter writer = r.getWriter();
-            writer.write(result);
+            writer.write(new SyndFeedOutput().outputString(feed));
             writer.flush();
             writer.close();
         } catch (Exception e) {
             log.error("Caught " + e.getClass().getName(), e);
             r.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
-        } finally {
-            IOUtils.closeQuietly(response);
         }
     }
 
