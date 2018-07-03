04098790f6c088cc06d9543be4ff8e565ed31aeb
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2018-07-03 15:05:30 +0200

    SALESDEMO-393 make Map component parameters part of a group

--- site/src/main/java/com/onehippo/gogreen/components/common/MapParamsInfo.java
+++ site/src/main/java/com/onehippo/gogreen/components/common/MapParamsInfo.java
@@ -1,13 +1,25 @@
 /*
- * Copyright 2011-2013 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2011-2018 Hippo B.V. (http://www.onehippo.com)
  */
 package com.onehippo.gogreen.components.common;
 
+import org.hippoecm.hst.core.parameters.FieldGroup;
+import org.hippoecm.hst.core.parameters.FieldGroupList;
 import org.hippoecm.hst.core.parameters.Parameter;
+
+import static com.onehippo.gogreen.components.common.MapParamsInfo.PARAM_ADDRESS;
+import static com.onehippo.gogreen.components.common.MapParamsInfo.PARAM_ZOOMLEVEL;
 
 /**
  * HST Component Parameters Info class for the Banner. Used by the PageComposer.
  */
+
+@FieldGroupList({
+        @FieldGroup(
+                titleKey = "settings",
+                value = {PARAM_ADDRESS, PARAM_ZOOMLEVEL}
+        )
+})
 public interface MapParamsInfo {
     String PARAM_ADDRESS = "address";
     String PARAM_ZOOMLEVEL = "zoomlevel";
