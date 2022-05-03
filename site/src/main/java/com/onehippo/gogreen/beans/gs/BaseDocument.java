4db1000667fceff4657ed78a6448820aaef426e4
Kenan Salic <kenan.salic@bloomreach.com>
2022-05-03 12:39:11 +0200

    bynder integration blueprint updates

--- site/src/main/java/com/onehippo/gogreen/beans/gs/BaseDocument.java
+++ site/src/main/java/com/onehippo/gogreen/beans/gs/BaseDocument.java
@@ -5,6 +5,8 @@
 package com.onehippo.gogreen.beans.gs;
 
 import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoAvailableTranslationsBean;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.content.beans.standard.HippoDocument;
 import org.hippoecm.repository.api.HippoNode;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
@@ -13,6 +15,9 @@ import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @HippoEssentialsGenerated(allowModifications = false)
 @Node(jcrType = "myproject:basedocument")
@@ -20,6 +25,12 @@ public class BaseDocument extends HippoDocument {
 
     public static final Logger log = LoggerFactory.getLogger(BaseDocument.class);
 
+    public Map<String, HippoBean> getTranslations() {
+        return super.getAvailableTranslations()
+                .getTranslations().stream()
+                .filter(hippoBean -> !hippoBean.getIdentifier().equals(getIdentifier()))
+                .collect(Collectors.toMap((entry) -> entry.getProperties().containsKey("hippotranslation:locale") ? entry.getSingleProperty("hippotranslation:locale") : "default", (entry) -> entry));
+    }
 
     public String getLastModifiedBy() {
         return getSingleProperty("hippostdpubwf:lastModifiedBy");
