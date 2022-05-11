e0663d27e4fa434575c06aae9f758b7fa7fc8929
Kenan Salic <kenan.salic@bloomreach.com>
2022-05-11 11:03:57 +0200

    frontend adjustments

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -12,6 +12,26 @@ export default function App({Component, pageProps}: AppProps): JSX.Element {
                 <meta charSet="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="shortcut icon" type="image/png" href="/favicon.png" sizes="64x64"/>
+
+                <link rel="preload" href="/css/fonts/barlow/barlow-v5-latin-ext_latin-regular.woff2" as="font"
+                      type="font/woff2" crossOrigin="anonymous"/>
+                <link rel="preload" href="/css/fonts/barlow/barlow-v5-latin-ext_latin-italic.woff2" as="font"
+                      type="font/woff2" crossOrigin="anonymous"/>
+                <link rel="preload" href="/css/fonts/barlow/barlow-v5-latin-ext_latin-500.woff2" as="font"
+                      type="font/woff2" crossOrigin="anonymous"/>
+                <link rel="preload" href="/css/fonts/barlow/barlow-v5-latin-ext_latin-500italic.woff2"
+                      as="font" type="font/woff2" crossOrigin="anonymous"/>
+                <link rel="preload" href="/css/fonts/barlow/barlow-v5-latin-ext_latin-600.woff2"
+                      as="font" type="font/woff2" crossOrigin="anonymous"/>
+                <link rel="preload"
+                      href="../assets/fonts/barlow/barlow-v5-latin-ext_latin-600italic.woff2"
+                      as="font" type="font/woff2" crossOrigin="anonymous"/>
+                <link rel="preload"
+                      href="../assets/fonts/barlow/barlow-v5-latin-ext_latin-700.woff2"
+                      as="font" type="font/woff2" crossOrigin="anonymous"/>
+                <link rel="preload"
+                      href="../assets/fonts/barlow/barlow-v5-latin-ext_latin-700italic.woff2"
+                      as="font" type="font/woff2" crossOrigin="anonymous"/>
                 <link rel="stylesheet"
                       href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700%7CPoppins:300,400,500,600,700"/>
                 <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"/>
